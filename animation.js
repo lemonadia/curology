b661457006fa962174864ad28503a1582acbaa2d
Nadia TUmidajska <nadia.tumidajska@gmail.com>
2020-04-16 17:39:08 +0200

    Add animation

---
+++ animation.js
@@ -0,0 +1,34 @@
+const animatedTags = document.querySelectorAll("h2, h3, img, p, a.button");
+
+// fade out on load
+
+animatedTags.forEach((tag) => {
+  tag.style.opacity = 0;
+});
+
+const fadeIn = function () {
+  //look at all the animated tags and see with the getBoundingClientRect if it is the window
+
+  let delay = 0.1;
+  animatedTags.forEach((tag) => {
+    const tagTop = tag.getBoundingClientRect().top;
+    const bottomTag = tag.getBoundingClientRect().bottom;
+    if (tagTop < window.innerHeight && bottomTag > 0) {
+      tag.style.animation = `fadein 1s ${delay}s both`;
+      delay = delay + 0.1;
+    } else {
+      tag.style.opacity = 0;
+      tag.style.animation = "";
+    }
+  });
+};
+
+fadeIn();
+
+document.addEventListener("scroll", function () {
+  fadeIn();
+});
+//on browser resize run fade in
+window.addEventListener("resize", function () {
+  fadeIn();
+});
